900bb3e79ee684ea0d976b5528b4460bfd1e3965
Omar Qureshi <omarqureshi786@hotmail.com>
2015-12-13 18:35:15 +0000

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+document.getElementById('thugitup').onclick = function() {
+    var div = $('#textbox').val();
+    var add = div + ", dawg";
+    document.getElementById('thugged').innerHTML = add; 
+}
